7ee8682a1c2677b28902a0463d7f81f898a5581f
Hrishav <164610722+HrishavSarma@users.noreply.github.com>
2024-03-24 00:35:02 +0530

    Add files via upload

---
+++ 1stGame/main.cpp
@@ -0,0 +1,32 @@
+#include"game.h"
+
+Game* game = nullptr;
+
+int main(int argc, char* argv[])
+{
+	const int FPS = 60;
+	const int framedelay = 1000 / FPS;
+
+	Uint32 frameStart;
+	int frameTime;
+
+	game = new Game();
+	
+	game->init("Game Engine", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 658, 368, false);
+	
+	while (game->running())
+	{
+		frameStart = SDL_GetTicks();
+		game->handlevents();
+		game->update();
+		game->render();
+		frameTime = SDL_GetTicks() - frameStart;
+		if (framedelay > frameTime)
+		{
+			SDL_Delay(framedelay - frameTime);
+		}
+
+	}
+	game->clean();
+	return 0;
+}
